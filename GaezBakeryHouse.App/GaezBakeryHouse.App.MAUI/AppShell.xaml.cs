4c39e44e3c587bb3c15685314069dc78aa66a53c
Carlos Manuel Torres Hipolito <a2193330294@alumnos.uat.edu.mx>
2023-09-03 11:32:44 -0600

    se agrega proyecto maui

---
+++ GaezBakeryHouse.App/GaezBakeryHouse.App.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GaezBakeryHouse.App.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
