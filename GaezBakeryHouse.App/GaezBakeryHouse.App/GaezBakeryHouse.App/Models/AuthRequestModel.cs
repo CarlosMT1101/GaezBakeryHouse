8ab5d5f6c1c8582fb33abdc99f57a6adb34593f0
Carlos Man <carlos@fakemail.com>
2023-11-10 20:02:07 -0600

    Se limpia el proyecto de xamarin forms

--- GaezBakeryHouse.App/GaezBakeryHouse.App/GaezBakeryHouse.App/Models/AuthRequestModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GaezBakeryHouse.App.Models
-{
-    public class AuthRequestModel
-    {
-        public string Email { get; set; }
-
-        public string Password { get; set; }
-    }
-}
