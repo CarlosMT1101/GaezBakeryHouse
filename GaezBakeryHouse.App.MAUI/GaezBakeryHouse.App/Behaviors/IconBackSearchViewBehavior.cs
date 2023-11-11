56065d9f067042007451f7cf4045f8372c386637
Carlos Man <carlos@fakemail.com>
2023-11-11 15:34:23 -0600

    IconBAckSearchViewBehavior funcionado

---
+++ GaezBakeryHouse.App.MAUI/GaezBakeryHouse.App/Behaviors/IconBackSearchViewBehavior.cs
@@ -0,0 +1,46 @@
+﻿using GaezBakeryHouse.App.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaezBakeryHouse.App.Behaviors
+{
+    public class IconBackSearchViewBehavior : Behavior<Image>
+    {
+        private TapGestureRecognizer _tapIconSearchBack;
+
+        public IconBackSearchViewBehavior() =>
+            _tapIconSearchBack = new TapGestureRecognizer { NumberOfTapsRequired = 1 };
+
+        protected override void OnAttachedTo(Image bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.Loaded += OnIconSearchBackLoaded;
+            _tapIconSearchBack.Tapped += OnIconSearchBackTapped;
+        }
+
+        private async void OnIconSearchBackTapped(object sender, TappedEventArgs e)
+        {
+            var iconSearch = sender as Image;
+            var contentGrid = (Grid)((ContentView)((Grid)iconSearch.Parent).Parent).Parent;
+            var searchView = (SearchView)contentGrid.Children[4];
+
+            await searchView.FadeTo(0, 100, Easing.Linear);
+            searchView.IsVisible = false;
+        }
+
+        protected override void OnDetachingFrom(Image bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.Loaded -= OnIconSearchBackLoaded;
+            _tapIconSearchBack.Tapped -= OnIconSearchBackTapped;
+        }
+
+        private void OnIconSearchBackLoaded(object sender, EventArgs e) =>
+            (sender as Image).GestureRecognizers.Add(_tapIconSearchBack);
+    }
+}
---
+++ GaezBakeryHouse.App.MAUI/GaezBakeryHouse.App/Behaviors/IconSearchBehavior.cs
@@ -0,0 +1,41 @@
+﻿using GaezBakeryHouse.App.Controls;
+
+namespace GaezBakeryHouse.App.Behaviors
+{
+    public class IconSearchBehavior : Behavior<Image>
+    {
+        private TapGestureRecognizer _tapSearchIcon;
+
+        public IconSearchBehavior() =>
+            _tapSearchIcon = new TapGestureRecognizer { NumberOfTapsRequired = 1 };
+
+        protected override void OnAttachedTo(Image bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.Loaded += OnIconSearchLoaded;
+            _tapSearchIcon.Tapped += OnSearchIconTapped;
+        }
+
+        private async void OnSearchIconTapped(object sender, TappedEventArgs e)
+        {
+            var iconSearch = sender as Image;
+            var contentGrid = (Grid)((ContentView)((Grid)iconSearch.Parent).Parent).Parent;
+            var searchView = (SearchView)contentGrid.Children[4];
+
+            searchView.IsVisible = true;
+            await searchView.FadeTo(1, 100, Easing.Linear);
+        }
+
+        protected override void OnDetachingFrom(Image bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.Loaded -= OnIconSearchLoaded;
+            _tapSearchIcon.Tapped -= OnSearchIconTapped;
+        }
+
+        private void OnIconSearchLoaded(object sender, EventArgs e) =>
+            (sender as Image).GestureRecognizers.Add(_tapSearchIcon);
+    }
+}
