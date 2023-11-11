32cd15cf6e5712e204cdcda9bb995f6ee5e15525
Carlos Man <carlos@fakemail.com>
2023-11-10 21:03:01 -0600

    Diseño LoginPage terminado

--- GaezBakeryHouse.App/GaezBakeryHouse.App/GaezBakeryHouse.App/App.xaml.cs
+++ GaezBakeryHouse.App/GaezBakeryHouse.App/GaezBakeryHouse.App/App.xaml.cs
@@ -1,7 +1,4 @@
-﻿using GaezBakeryHouse.App.Models;
-using GaezBakeryHouse.App.Views;
-using System;
-using Xamarin.Essentials;
+﻿using GaezBakeryHouse.App.Views;
 using Xamarin.Forms;
 
 
@@ -12,83 +9,83 @@ namespace GaezBakeryHouse.App
         public App()
         {
             InitializeComponent();
-            MainPage = new AppShell();
+            MainPage = new LoginPage();
         }
 
-        protected async override void OnStart()
-        {
-            var accessToken = await SecureStorage.GetAsync(Constants.AccessToken);
-            var expirationToken = await SecureStorage.GetAsync(Constants.ExpirationToken);
+        //protected async override void OnStart()
+        //{
+        //    var accessToken = await SecureStorage.GetAsync(Constants.AccessToken);
+        //    var expirationToken = await SecureStorage.GetAsync(Constants.ExpirationToken);
 
-            if (accessToken != null)
-            {
-                var expirationDate = DateTime.Parse(expirationToken);
+        //    if (accessToken != null)
+        //    {
+        //        var expirationDate = DateTime.Parse(expirationToken);
 
-                if (DateTime.UtcNow < expirationDate)
-                {
-                    await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
-                }
-                else
-                {
-                    App.RemoveUserInformation();
-                    await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
-                }
-            }
-            else
-            {
-                await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
-            }
-        }
+        //        if (DateTime.UtcNow < expirationDate)
+        //        {
+        //            await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+        //        }
+        //        else
+        //        {
+        //            App.RemoveUserInformation();
+        //            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        //    }
+        //}
 
-        public static async void SaveUserInformation(AuthResponseModel authResponseModel)
-        {
-            await SecureStorage.SetAsync(Constants.FullName, $"{authResponseModel.FullName}");
-            await SecureStorage.SetAsync(Constants.PhoneNumber, $"{authResponseModel.PhoneNumber}");
-            await SecureStorage.SetAsync(Constants.LastName, $"{authResponseModel.LastName}");
-            await SecureStorage.SetAsync(Constants.UserName, $"{authResponseModel.UserName}");
-            await SecureStorage.SetAsync(Constants.AccessToken, $"{Constants.Bearer} {authResponseModel.Token}");
-            await SecureStorage.SetAsync(Constants.ExpirationToken, authResponseModel.Expiration.ToString());
-            await SecureStorage.SetAsync(Constants.ApplicationUserId, authResponseModel.ApplicationUserId);
-        }
+        //public static async void SaveUserInformation(AuthResponseModel authResponseModel)
+        //{
+        //    await SecureStorage.SetAsync(Constants.FullName, $"{authResponseModel.FullName}");
+        //    await SecureStorage.SetAsync(Constants.PhoneNumber, $"{authResponseModel.PhoneNumber}");
+        //    await SecureStorage.SetAsync(Constants.LastName, $"{authResponseModel.LastName}");
+        //    await SecureStorage.SetAsync(Constants.UserName, $"{authResponseModel.UserName}");
+        //    await SecureStorage.SetAsync(Constants.AccessToken, $"{Constants.Bearer} {authResponseModel.Token}");
+        //    await SecureStorage.SetAsync(Constants.ExpirationToken, authResponseModel.Expiration.ToString());
+        //    await SecureStorage.SetAsync(Constants.ApplicationUserId, authResponseModel.ApplicationUserId);
+        //}
 
-        public static void RemoveUserInformation()
-        {
-            SecureStorage.Remove(Constants.UserName);
-            SecureStorage.Remove(Constants.PhoneNumber);
-            SecureStorage.Remove(Constants.FullName);
-            SecureStorage.Remove(Constants.LastName);
-            SecureStorage.Remove(Constants.AccessToken);
-            SecureStorage.Remove(Constants.ExpirationToken);
-            SecureStorage.Remove(Constants.ApplicationUserId);
-        }
+        //public static void RemoveUserInformation()
+        //{
+        //    SecureStorage.Remove(Constants.UserName);
+        //    SecureStorage.Remove(Constants.PhoneNumber);
+        //    SecureStorage.Remove(Constants.FullName);
+        //    SecureStorage.Remove(Constants.LastName);
+        //    SecureStorage.Remove(Constants.AccessToken);
+        //    SecureStorage.Remove(Constants.ExpirationToken);
+        //    SecureStorage.Remove(Constants.ApplicationUserId);
+        //}
 
-        protected override void OnSleep()
-        {
-        }
+        //protected override void OnSleep()
+        //{
+        //}
 
-        protected async override void OnResume()
-        {
-            var accessToken = await SecureStorage.GetAsync(Constants.AccessToken);
-            var expirationToken = await SecureStorage.GetAsync(Constants.ExpirationToken);
+        //protected async override void OnResume()
+        //{
+        //    var accessToken = await SecureStorage.GetAsync(Constants.AccessToken);
+        //    var expirationToken = await SecureStorage.GetAsync(Constants.ExpirationToken);
 
-            if (accessToken != null)
-            {
-                var expirationDate = DateTime.Parse(expirationToken);
+        //    if (accessToken != null)
+        //    {
+        //        var expirationDate = DateTime.Parse(expirationToken);
 
-                if (DateTime.UtcNow < expirationDate)
-                {
-                    await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
-                }
-                else
-                {
-                    App.RemoveUserInformation();
-                    await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
-                }
-            }
-            else
-            {
-                await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
-            }
-        }
+        //        if (DateTime.UtcNow < expirationDate)
+        //        {
+        //            await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+        //        }
+        //        else
+        //        {
+        //            App.RemoveUserInformation();
+        //            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        //        }
+        //    }
+        //    else
+        //    {
+        //        await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        //    }
+        //}
     }
 }
---
+++ GaezBakeryHouse.App/GaezBakeryHouse.App/GaezBakeryHouse.App/Views/LoginPageFolder/LoginBoxView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GaezBakeryHouse.App.Views.LoginPageFolder
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginBoxView : ContentView
+    {
+        public LoginBoxView()
+        {
+            InitializeComponent();
+        }
+    }
+}
